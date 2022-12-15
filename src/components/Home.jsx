135bd46ba1318727a718e20ce209804d309cf5c2
IIsotope <isotopegayina606@gmail.com>
2022-12-15 01:29:38 +0000

    slight

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
       <section id="home" className='flex flex-col p-5  items-center w-[100vw] h-[50vh] bg-orange-300'>
         <div></div>
         <div className="mt-[10vh] text-xl font-bold uppercase">Enter Tracking / Container Number</div>
-        <div className="mt-5 flex gap-5">
+        <div className="mt-5 flex sm:flex-row flex-col gap-5">
             <input placeholder="Tracking Number #"  className="border-gray-300 w-[70vw] h-[5vh] rounded p-3"/>
             <button type="submit" className="pl-2 pr-2 bg-orange-700 text-white font-bold">Track & Trace</button>
             
